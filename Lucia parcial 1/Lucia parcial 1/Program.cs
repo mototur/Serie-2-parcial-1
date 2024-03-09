5bdf4eeac29b7f7b9457a06d9df24fd776c2085a
Lucia Sandoval <asandovalp9@miumg.edu.gt>
2024-03-09 12:19:43 -0600

    Commit 1 primer parcial
    Lucia

---
+++ Lucia parcial 1/Lucia parcial 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucia_parcial_1
+{
+    internal class Program
+
+    {
+        static long programa;
+        static string pro = "primer parcial de programcion: ";
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Primer parcial de programacion");
+        }
+    }
+}
